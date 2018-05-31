4730c37086a64c25133d40827c5050a6f467c6fb
Ron <ron@drongowski.de>
2018-05-31 10:38:08 +0200

    Reorganize vcl template structure

--- components/varnish/component.py
+++ components/varnish/component.py
@@ -7,8 +7,20 @@ class Varnish(Component):
 
     def configure(self):
         self += File(
-            "default.vcl", source="default.vcl.tpl", is_template="true")
+            "default.vcl",
+            source="default.vcl",
+            is_template="true")
+
         self += Directory('vcl_includes')
+
+        self += File(
+             "vcl_includes/acl.vcl",
+            source="vcl_includes/acl.vcl",
+            is_template="true")
+        self += File(
+            "vcl_includes/backends.vcl",
+            source="vcl_includes/backends.vcl",
+            is_template="true")
         self += File(
             "vcl_includes/backend_response.vcl",
             source="vcl_includes/backend_response.vcl",
@@ -18,20 +30,25 @@ class Varnish(Component):
             source="vcl_includes/backend_fetch.vcl",
             is_template="true")
         self += File(
-            "vcl_includes/synth.vcl",
-            source="vcl_includes/synth.vcl",
+            "vcl_includes/deliver.vcl",
+            source="vcl_includes/deliver.vcl",
+            is_template="true")
+        self += File(
+            "vcl_includes/init.vcl",
+            source="vcl_includes/init.vcl",
             is_template="true")
         self += File(
             "vcl_includes/main.vcl",
             source="vcl_includes/main.vcl",
             is_template="true")
         self += File(
-            "vcl_includes/deliver.vcl",
-            source="vcl_includes/deliver.vcl",
-            is_template="true")
-        self += File(
             "vcl_includes/recv.vcl",
             source="vcl_includes/recv.vcl",
             is_template="true")
+        self += File(
+            "vcl_includes/synth.vcl",
+            source="vcl_includes/synth.vcl",
+            is_template="true")
+
         self.provide('varnish_dir', self.workdir)
 
--- components/varnish/vcl_includes/main.vcl
+++ components/varnish/vcl_includes/main.vcl
@@ -1,3 +1,4 @@
+include "/etc/varnish/vcl_includes/acl.vcl";
 include "/etc/varnish/vcl_includes/recv.vcl";
 include "/etc/varnish/vcl_includes/backend_response.vcl";
 include "/etc/varnish/vcl_includes/synth.vcl";
