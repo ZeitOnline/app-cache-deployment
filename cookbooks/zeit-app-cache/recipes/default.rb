381b4309dc99d0889e71979e1917a3f05842a19e
Ron <ron@drongowski.de>
2018-06-20 14:07:16 +0200

    Connect chef and batou

--- cookbooks/zeit-app-cache/recipes/default.rb
+++ cookbooks/zeit-app-cache/recipes/default.rb
@@ -1,1 +1,22 @@
+# DO NOT ACTUALLY USE THIS IN PRODUCTION
+#
+# This recipe is only meant to do some boostrapping in development
+
 apt_update "update"
+
+directory '/srv/app-cache/deployment/work/varnish/vcl_includes' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+file '/srv/app-cache/deployment/work/varnish/default.vcl' do
+  content "vcl 4.0;\nbackend ap1 {\n  .host = \"127.0.0.1\";\n  .port = \"8080\";\n}"
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
--- cookbooks/zeit-app-cache/recipes/varnish.rb
+++ cookbooks/zeit-app-cache/recipes/varnish.rb
@@ -1,12 +1,12 @@
 package "varnish"
 
-service "varnish" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action [ :enable ]
+execute 'varnish_restart' do
+  command 'systemctl daemon-reload && systemctl restart varnish'
+  action :nothing
 end
 
 execute 'varnish_reload' do
-  command 'systemctl daemon-reload && systemctl restart varnish'
+  command 'systemctl reload varnish'
   action :nothing
 end
 
@@ -24,3 +24,25 @@ template "/etc/systemd/system/varnish.service.d/varnish.conf" do
   mode "0644"
   notifies :run, 'execute[varnish_reload]', :immediately
 end
+
+# Link VCL to configured batou location
+varnish_config = node["batou"]["varnish_config"] or []
+if ! varnish_config.is_a?(Array)
+  varnish_config = [varnish_config]
+end
+
+varnish_config.each do |config|
+  link "/etc/varnish/#{File.basename(config)}" do
+    to config
+	only_if { File.exists? config or Directory.exists? config}
+    notifies :run, "execute[varnish_reload]", :delayed
+  end
+end
+
+# Allow batou to restart varnish,if serviceUser is given
+template "/etc/sudoers.d/batou-varnish" do
+  source "sudoers.erb"
+  mode "0440"
+  only_if { Dir.exists? "/etc/sudoers.d" }
+end
+
