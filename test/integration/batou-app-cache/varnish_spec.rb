ced98c584bc3f64195223236f2fc4f49707eb93e
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-10-25 11:37:42 +0200

    ZON-4963: Group tests

--- test/integration/batou-app-cache/varnish_spec.rb
+++ test/integration/batou-app-cache/varnish_spec.rb
@@ -1,23 +1,25 @@
-# encoding: utf-8
-# author: Ron Drongowski
+control "varnish package is installed" do
+  describe package('varnish') do
+    it { should be_installed }
+  end
 
-describe package('varnish') do
-  it { should be_installed }
+  describe processes('varnishd') do
+    its('list.length') { should eq 2 }
+    its('commands') { should include a_string_matching("-a :8080") }
+  end
 end
 
-describe processes('varnishd') do
-  its('list.length') { should eq 2 }
-  its('commands') { should include a_string_matching("-a :8080") }
-end
 
-describe file('/etc/varnish/default.vcl') do
-  its('link_path') { should eq '/srv/app-cache/deployment/work/varnish/default.vcl' }
-end
+control "vcl config is deployed" do
+  describe file('/etc/varnish/default.vcl') do
+    its('link_path') { should eq '/srv/app-cache/deployment/work/varnish/default.vcl' }
+  end
 
-describe file('/etc/varnish/vcl_includes') do
-  its('link_path') { should eq '/srv/app-cache/deployment/work/varnish/vcl_includes' }
-end
+  describe file('/etc/varnish/vcl_includes') do
+    its('link_path') { should eq '/srv/app-cache/deployment/work/varnish/vcl_includes' }
+  end
 
-describe file('/etc/sudoers.d/batou-varnish') do
-    its('content') { should match(%r{app-cache.*NOPASSWD.*varnish.service}) }
+  describe file('/etc/sudoers.d/batou-varnish') do
+      its('content') { should match(%r{app-cache.*NOPASSWD.*varnish.service}) }
+  end
 end
