b0bb8ed04b7bd5f5dcbc240764dc32d6a752506d
Ron <ron@drongowski.de>
2018-11-26 18:02:26 +0100

    Add new service definition and pin varnish version

--- cookbooks/zeit-app-cache/attributes/varnish.rb
+++ cookbooks/zeit-app-cache/attributes/varnish.rb
@@ -3,4 +3,4 @@ default["varnish"]["port"] = "8080"
 default["varnish"]["storage-size"] = "256M"
 default["varnish"]["send-timeout"] = "60"
 default["varnish"]["enable_ncsa_service"] = false
-
+default["varnish"]["package-version"] = "6.0.2-1~bionic"
