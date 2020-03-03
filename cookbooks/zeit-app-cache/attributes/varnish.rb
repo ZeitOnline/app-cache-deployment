6fedde8a36b40c7350d76331bba9d2f4a8e6fa43
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2020-03-03 15:54:02 +0100

    OPS-1181: Make workspace_client configurable
    If we want more parameters, we'll probably should think about a dict
    attribute and then generating the commandline generically...

--- cookbooks/zeit-app-cache/attributes/varnish.rb
+++ cookbooks/zeit-app-cache/attributes/varnish.rb
@@ -3,6 +3,7 @@ default["varnish"]["version"] = "6.3"
 default["varnish"]["port"] = "8080"
 default["varnish"]["storage-size"] = "256M"
 default["varnish"]["send-timeout"] = "60"
+default["varnish"]["workspace-client"] = "64k"
 # To get the upstream default format, set to this:
 # '%h %l %u %t "%r" %s %b "%{Referer}i" "%{User-agent}i"'
 default["varnish"]["ncsa_format"] = false
